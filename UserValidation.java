cda7c7083d402422288c67407fe5c8c9d86e009d
amod1811 <ammch101@gmail.com>
2022-05-16 23:52:21 +0530

    Junit test Happy or SAD for Entry Sccessfully or Fail test casr

---
+++ UserValidation.java
@@ -0,0 +1,22 @@
+package bl.com.junituserregisration;
+
+public class UserValidation {
+    public boolean validName(String Amod) {
+        return true;
+    }
+
+    public boolean validEmail(String email) {
+        return true;
+    }
+
+    public boolean validMobile(String mobile) {
+        return  true;
+    }
+
+    public boolean validPassword(String password) {
+        return true;
+    }
+    public boolean validName(String name) {
+        return true;
+    }
+}
